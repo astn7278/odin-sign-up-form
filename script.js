dcfa28f095332ee541b3a0a87439795341356116
Austin Fletcher <49542016+astn7278@users.noreply.github.com>
2024-02-18 21:23:35 -0700

    Apply match password logic to update html and css

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let pass1Value;
-let pass2Value;
+let pass1Value = '';
+let pass2Value = '';
 
 let pass1 = document.getElementById('password');
 pass1.addEventListener('input', function(event) {
@@ -13,9 +13,25 @@ pass2.addEventListener('input', function(event) {
     passwordCompare();
 })
 
+let passError = document.getElementById('passError');
+let passFields = document.querySelectorAll('input[type="password"]')
+
 function passwordCompare() {
     if (pass1Value !== pass2Value) {
-        console.log("FUCK")
-    } else console.log("YAY")
+        passError.textContent = 'passwords dont match';
+        passFields.forEach(function(passField) {
+            passField.style.border = "2px solid rgb(174, 0, 0)"
+        });
+    } else if (pass1Value === pass2Value && pass1Value !== '' && pass2Value !== '') {
+        passError.textContent = 'looks good';
+        passError.style.color = "#537c37";
+        passFields.forEach(function(passField) {
+            passField.style.border = "2px solid #537c37"
+        })
+    } else if (pass1Value === '' && pass2Value === '') {
+        passFields.forEach(function(passField) {
+            passError.textContent = '';
+            passField.style.border = "";
+        })   
     }
-
+}
